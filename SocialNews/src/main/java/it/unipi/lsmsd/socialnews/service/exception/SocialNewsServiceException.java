1730d469dcff9e8ee65fe430493a1bc548f44406
Federico Cristofani <84182920+federico-cristofani@users.noreply.github.com>
2023-02-01 17:10:21 +0100

    Service custom exception

---
+++ SocialNews/src/main/java/it/unipi/lsmsd/socialnews/service/exception/SocialNewsServiceException.java
@@ -0,0 +1,7 @@
+package it.unipi.lsmsd.socialnews.service.exception;
+
+public class SocialNewsServiceException extends Exception{
+    public SocialNewsServiceException(String message){
+        super(message);
+    }
+}
