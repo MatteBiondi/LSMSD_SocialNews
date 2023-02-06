367a26cd658003c42c90f3a8b2882b3a4003505c
Matteo Biondi <matteobiondi1197@gmail.com>
2023-02-06 12:36:25 +0100

    Reader services added

--- SocialNews/src/main/java/it/unipi/lsmsd/socialnews/service/ReaderService.java
+++ SocialNews/src/main/java/it/unipi/lsmsd/socialnews/service/ReaderService.java
@@ -1,7 +1,11 @@
 package it.unipi.lsmsd.socialnews.service;
 
 import it.unipi.lsmsd.socialnews.dto.ReaderDTO;
+import it.unipi.lsmsd.socialnews.dto.ReportDTO;
+import it.unipi.lsmsd.socialnews.dto.ReporterDTO;
 import it.unipi.lsmsd.socialnews.service.exception.SocialNewsServiceException;
+
+import java.util.List;
 
 public interface ReaderService {
     /**
@@ -31,4 +35,54 @@ public interface ReaderService {
      */
     ReaderDTO readerInfo(String email) throws SocialNewsServiceException;
 
+    /**
+     * Publish a report related to a selected post
+     *
+     * @param newReport DTO object containing the information of the new report to publish
+     * @return the identifier assigned to the new report
+     * @throws SocialNewsServiceException in case of failure of the operation
+     */
+    Long publishReport(ReportDTO newReport) throws SocialNewsServiceException;
+
+    /**
+     * A reader (identify by id) starts to follow a reporter (identify by id)
+     *
+     * @param readerId id of the reader that starts to follow the reporter
+     * @param reporterId id of the reporter that is followed
+     * @return number of following relationship added to the database
+     * @throws SocialNewsServiceException in case of failure of the creation operation on database
+     */
+    Integer followReporter(String readerId, String reporterId) throws SocialNewsServiceException;
+
+    /**
+     * Retrieve a list of the followed reporters, ordered by id, of a specified reader, up to a configured number of
+     * reporters
+     *
+     * @param readerId id of the reader for which retrieve the followed reporters
+     * @return list of reporterDTO objects containing basic information
+     * @throws SocialNewsServiceException in case of failure of the operation
+     */
+    List<ReporterDTO> firstPageFollowing(String readerId) throws SocialNewsServiceException;
+
+    /**
+     * Retrieve a list of the followed reporters, ordered by id, of a specified reader, up to a configured number of
+     * reporters and starting from a certain offset
+     *
+     * @param readerId id of the reader for which retrieve the followed reporters
+     * @param followingOffset integer containing the number of the last reporter in the previous page with respect the total
+     *                        number of results
+     * @return list of reporterDTO objects containing basic information
+     * @throws SocialNewsServiceException in case of failure of the operation
+     */
+    List<ReporterDTO> nextPageFollowing(String readerId, Integer followingOffset) throws SocialNewsServiceException;
+
+    /**
+     * A reader (identify by id) ends to follow a reporter (identify by id)
+     *
+     * @param readerId id of the reader that ends to follow the reporter
+     * @param reporterId id of the reporter that is followed
+     * @return number of following relationship removed from the database
+     * @throws SocialNewsServiceException in case of failure of the deletion operation on database
+     */
+    Integer unfollowReporter(String readerId, String reporterId) throws SocialNewsServiceException;
 }
--- SocialNews/src/main/java/it/unipi/lsmsd/socialnews/service/implement/ReaderServiceImpl.java
+++ SocialNews/src/main/java/it/unipi/lsmsd/socialnews/service/implement/ReaderServiceImpl.java
@@ -4,11 +4,15 @@ import it.unipi.lsmsd.socialnews.dao.DAOLocator;
 import it.unipi.lsmsd.socialnews.dao.exception.SocialNewsDataAccessException;
 import it.unipi.lsmsd.socialnews.dao.model.Reader;
 import it.unipi.lsmsd.socialnews.dto.ReaderDTO;
+import it.unipi.lsmsd.socialnews.dto.ReportDTO;
+import it.unipi.lsmsd.socialnews.dto.ReporterDTO;
 import it.unipi.lsmsd.socialnews.service.ReaderService;
 import it.unipi.lsmsd.socialnews.service.exception.SocialNewsServiceException;
 import it.unipi.lsmsd.socialnews.service.util.Util;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReaderServiceImpl implements ReaderService {
     /**
@@ -74,6 +78,111 @@ public class ReaderServiceImpl implements ReaderService {
         catch (IllegalArgumentException ex){
             ex.printStackTrace();
             throw new SocialNewsServiceException("User not in the system, check the email field");
+        }
+    }
+
+    /**
+     * Publish a report related to a selected post
+     *
+     * @param newReport DTO object containing the information of the new report to publish
+     * @return the identifier assigned to the new report
+     * @throws SocialNewsServiceException in case of failure of the operation
+     */
+    @Override
+    public Long publishReport(ReportDTO newReport) throws SocialNewsServiceException{
+        try {
+            return DAOLocator.getReportDAO().addReport(Util.buildReport(newReport));
+        } catch (SocialNewsDataAccessException ex) {
+            ex.printStackTrace();
+            throw new SocialNewsServiceException("Database error");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            throw new SocialNewsServiceException("Error in report publication. Retry again.");
+        }
+    }
+
+    /**
+     * A reader (identify by id) starts to follow a reporter (identify by id)
+     *
+     * @param readerId id of the reader that starts to follow the reporter
+     * @param reporterId id of the reporter that is followed
+     * @return number of following relationship added to the database
+     * @throws SocialNewsServiceException in case of failure of the creation operation on database
+     */
+    @Override
+    public Integer followReporter(String readerId, String reporterId) throws SocialNewsServiceException{
+        try {
+            return DAOLocator.getReaderDAO().followReporter(readerId, reporterId);
+        } catch (SocialNewsDataAccessException ex) {
+            ex.printStackTrace();
+            throw new SocialNewsServiceException("Database error");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            throw new SocialNewsServiceException("Error in follow operation. Retry again.");
+        }
+    }
+
+    /**
+     * Retrieve a list of the followed reporters, ordered by id, of a specified reader, up to a configured number of
+     * reporters
+     *
+     * @param readerId id of the reader for which retrieve the followed reporters
+     * @return list of reporterDTO objects containing basic information
+     * @throws SocialNewsServiceException in case of failure of the operation
+     */
+    @Override
+    public List<ReporterDTO> firstPageFollowing(String readerId) throws SocialNewsServiceException{
+        return nextPageFollowing(readerId, 0);
+    }
+
+    /**
+     * Retrieve a list of the followed reporters, ordered by id, of a specified reader, up to a configured number of
+     * reporters and starting from a certain offset
+     *
+     * @param readerId id of the reader for which retrieve the followed reporters
+     * @param followingOffset integer containing the number of the last reporter in the previous page with respect the total
+     *                        number of results
+     * @return list of reporterDTO objects containing basic information
+     * @throws SocialNewsServiceException in case of failure of the operation
+     */
+    @Override
+    public List<ReporterDTO> nextPageFollowing(String readerId, Integer followingOffset) throws SocialNewsServiceException{
+        try {
+            followingOffset = followingOffset != null ? followingOffset : Integer.valueOf(0);
+            List<ReporterDTO> firstPageReporterDTO = new ArrayList<>();
+            DAOLocator.getReaderDAO()
+                    .getFollowingByReaderId(readerId,
+                            Util.getIntProperty("listFollowingPageSize",25),
+                            followingOffset )
+                    .forEach(reporter -> firstPageReporterDTO.add(Util.buildReporterDTO(reporter)));
+            return firstPageReporterDTO;
+        } catch (SocialNewsDataAccessException ex) {
+            ex.printStackTrace();
+            throw new SocialNewsServiceException("Database error");
+        }
+    }
+
+    /**
+     * A reader (identify by id) ends to follow a reporter (identify by id)
+     *
+     * @param readerId id of the reader that ends to follow the reporter
+     * @param reporterId id of the reporter that is followed
+     * @return number of following relationship removed from the database
+     * @throws SocialNewsServiceException in case of failure of the deletion operation on database
+     */
+    @Override
+    public Integer unfollowReporter(String readerId, String reporterId) throws SocialNewsServiceException{
+        try {
+            return DAOLocator.getReaderDAO().unfollowReporter(readerId, reporterId);
+        } catch (SocialNewsDataAccessException ex) {
+            ex.printStackTrace();
+            throw new SocialNewsServiceException("Database error");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            throw new SocialNewsServiceException("Error in unfollow operation. Retry again.");
         }
     }
 }
