941181f07554ea819f409f130aefb035052440b1
MartiB28 <martina.burgisi@gmail.com>
2023-02-08 12:22:54 +0100

    ReporterHomepageServlet basic implementation [WIP]

--- SocialNews/src/main/java/it/unipi/lsmsd/socialnews/servlet/ReporterHomepageServlet.java
+++ SocialNews/src/main/java/it/unipi/lsmsd/socialnews/servlet/ReporterHomepageServlet.java
@@ -1,11 +1,22 @@
 package it.unipi.lsmsd.socialnews.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.logging.Logger;
 
-@WebServlet(name = "ReporterHomepageServlet", value = "/reporterHomepage", loadOnStartup = 0)
+@WebServlet(name = "ReporterHomepageServlet", value = "/reporterhomepage", loadOnStartup = 0)
 public class ReporterHomepageServlet extends HttpServlet {
 
     private static final Logger LOGGER = Logger.getLogger(ReporterHomepageServlet.class.getName());
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        String targetJSP = "/pages/jsp/reporterHomepage.jsp";
+        request.getRequestDispatcher(targetJSP).forward(request, response);
+    }
 }
