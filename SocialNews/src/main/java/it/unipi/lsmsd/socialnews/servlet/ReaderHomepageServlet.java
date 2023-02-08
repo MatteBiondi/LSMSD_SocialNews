6167df32b09390eda4754cad6a5aefc54475d15f
MartiB28 <martina.burgisi@gmail.com>
2023-02-08 12:20:48 +0100

    ReaderHomepageServlet basic implementation [WIP]

--- SocialNews/src/main/java/it/unipi/lsmsd/socialnews/servlet/ReaderHomepageServlet.java
+++ SocialNews/src/main/java/it/unipi/lsmsd/socialnews/servlet/ReaderHomepageServlet.java
@@ -1,11 +1,22 @@
 package it.unipi.lsmsd.socialnews.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @WebServlet(name = "ReaderHomepageServlet", value = "/readerHomepage", loadOnStartup = 0)
 public class ReaderHomepageServlet extends HttpServlet {
 
     private static final Logger LOGGER = Logger.getLogger(ReaderHomepageServlet.class.getName());
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        String TargetJSP = "/pages/jsp/readerHomepage.jsp";
+        request.getRequestDispatcher(TargetJSP).forward(request, response);
+    }
 }
