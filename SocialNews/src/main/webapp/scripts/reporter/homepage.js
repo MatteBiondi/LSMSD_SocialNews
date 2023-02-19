906af50b2a278da9a9309817e881c09eae61e504
MartiB28 <martina.burgisi@gmail.com>
2023-02-19 16:15:47 +0100

    Post handling functionalities

--- SocialNews/src/main/webapp/scripts/reporter/homepage.js
+++ SocialNews/src/main/webapp/scripts/reporter/homepage.js
@@ -1,18 +1,120 @@
 function publishNewPost(reporterID) {
-    console.log(reporterID);
     let textarea = $("textarea").val();
     let hashtags = $("#hashtags-input").val();
     let links = $("#related-links-input").val();
     $.post(
-        "publishNewPost.jsp",
+        "posthandling",
         {
             reporterID: reporterID,
+            operation: "insert",
             text: textarea,
             hashtags: hashtags,
             links: links
         },
-        function(postID) {
-            //todo
+        function(newPostJson) {
+            cleanNewPostForm();
+            try {
+                let postID = newPostJson["reporterId"];
+                let textarea = newPostJson["text"];
+                let hashtags = newPostJson["hashtags"];
+                let links = newPostJson["links"];
+                createNewPost(reporterID, postID, textarea, hashtags, links);
+            } catch (e) {
+                console.error("Error parsing JSON: " + e);
+            }
         }
-    );
+    ).fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("AJAX request failed: " + errorThrown);
+    });
+}
+
+function cleanNewPostForm() {
+    $("textarea").val("");
+    $("#hashtags-input").val("");
+    $("#related-links-input").val("");
+}
+
+function createNewPost(reporterID, postID, text, hashtags, links) {
+
+    var newPost = $("<div></div>");
+    newPost.attr({
+       "class": "post-container container my-5 search-result",
+        "id": postID,
+    });
+
+    var postHeader = $("<header></header>");
+    postHeader.attr("class","post-header");
+    newPost.append(postHeader);
+
+    var removeSpan = $("<span></span>")
+    removeSpan.attr("class","option");
+    removeSpan.attr("data-ref", postID);
+    postHeader.append(removeSpan);
+
+    var removeIcon = $("<i></i>");
+    removeIcon.attr("class","bi bi-trash3");
+    removeIcon.click(removePost(reporterID, postID));
+    removeSpan.append(removeIcon);
+
+    newPost.append($("<hr>"));
+
+    var postText = $("<p>" + text + "</p>");
+    postText.attr("class","post-text");
+    newPost.append(postText);
+
+    var postFooter = $("<footer></footer>");
+    newPost.append(postFooter);
+
+    var hashtagsString = hashtags.toString();
+    if(hashtagsString !== "") {
+        hashtagsString = hashtagsString.replace(/,/g, " ");
+
+        var postHashtags = $("<p>" + hashtagsString + "</p>");
+        postHashtags.attr("class","hashtags");
+        postFooter.append(postHashtags);
+    }
+
+    var linksString = links.toString();
+    if(linksString !== "") {
+        linksString = linksString.replace(/,/g, " ");
+
+        var postLinks = $("<p>" + linksString + "</p>");
+        postLinks.attr("class","related-links");
+        postFooter.append(postLinks);
+    }
+
+    var postTimestamp = $("<p>Now</p>");
+    postTimestamp.attr("class","timestamp");
+    postFooter.append(postTimestamp);
+
+    postFooter.append($("<hr>"));
+
+    var showCommentsDiv = $("<div></div>");
+    showCommentsDiv.attr("class","show-comm-div");
+    postFooter.append(showCommentsDiv);
+
+    var showCommentsButton = $("<button>Show comments</button>");
+    showCommentsButton.attr("class","show-comm");
+    showCommentsDiv.append(showCommentsButton);
+
+    $("#post-items").prepend(newPost);
+}
+
+function removePost(reporterID, postID) {
+    $.post(
+        "posthandling",
+        {
+            reporterID: reporterID,
+            operation: "remove",
+            postID: postID
+        }
+    ).done(function() {
+        hideRemovedPost(postID);
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("AJAX request failed: " + errorThrown);
+    });
+}
+
+function hideRemovedPost(postID) {
+    $("#" + postID).remove();
 }
