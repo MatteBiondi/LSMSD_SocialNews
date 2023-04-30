d6aab41555889fb28093f572c5c2feb49371fd02
Matteo Biondi <matteobiondi1197@gmail.com>
2023-04-30 17:40:43 +0200

    Minor changes

--- SocialNews/src/main/java/it/unipi/lsmsd/socialnews/servlet/reader/ReportPostServlet.java
+++ SocialNews/src/main/java/it/unipi/lsmsd/socialnews/servlet/reader/ReportPostServlet.java
@@ -24,7 +24,7 @@ public class ReportPostServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/plain");
 
         // Take post id, reason and reporterId from request
