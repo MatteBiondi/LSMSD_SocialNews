53ca0b5b0b2fed8f07f994ecb6f5eb20c63f3c12
Federico Cristofani <federico.cristofani98@outlook.it>
2023-05-17 17:59:53 +0200

    Code cleanup

--- SocialNews/src/test/java/it/unipi/lsmsd/socialnews/dao/mongodb/MongoTest.java
+++ SocialNews/src/test/java/it/unipi/lsmsd/socialnews/dao/mongodb/MongoTest.java
@@ -1,12 +1,6 @@
 package it.unipi.lsmsd.socialnews.dao.mongodb;
 
-import it.unipi.lsmsd.socialnews.dao.exception.SocialNewsDataAccessException;
 import it.unipi.lsmsd.socialnews.dao.redundancy.RedundancyUpdater;
-import it.unipi.lsmsd.socialnews.service.PostService;
-import it.unipi.lsmsd.socialnews.service.ReporterService;
-import it.unipi.lsmsd.socialnews.service.exception.SocialNewsServiceException;
-import it.unipi.lsmsd.socialnews.service.implement.PostServiceImpl;
-import it.unipi.lsmsd.socialnews.service.implement.ReporterServiceImpl;
 import it.unipi.lsmsd.socialnews.service.util.Statistic;
 import it.unipi.lsmsd.socialnews.service.util.Util;
 import it.unipi.lsmsd.socialnews.servlet.admin.UsersServlet;
@@ -17,11 +11,6 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.List;
 import java.util.Properties;
 
 public class MongoTest {
